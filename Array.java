5617f31823d775dc57ffc1910d016f548f84cdc0
LumiKaten <zwang067@students.fairmontschools.com>
2025-02-19 13:29:27 -0800

    Update Array.java

--- Array.java
+++ Array.java
@@ -7,7 +7,15 @@ public class Array
 	// 7 8 9
 	public int[][] getRowMajorSquare(int x)
 	{
-//	
+		int count =1;
+		int [][] a= new int[x][x];
+		for(int row=0; row<x;row++){
+			for(int col =0; col<x; col++);
+			a[row][col] = count;
+			count++;
+
+		}
+	return a;
 	}
 
 	// Array should be of format
@@ -16,7 +24,17 @@ public class Array
 	// 3 6 9
 	public int[][] getColumnMajorSquare(int x)
 	{
-//		
+		int count =1;
+		int [][] a= new int[x][x];
+		for(int row=0; row<x;row++){
+			for(int col =0; col<x;col++);
+			a[col][row]=count;
+			count++;
+			
+
+		}
+
+	return a;
 	}
 
 	// Array should be of format
@@ -25,7 +43,15 @@ public class Array
 	// 9 10 11 12
 	public int[][] getRowMajorRectangle(int row, int column)
 	{
-		
+		int count=1;
+		int [][] a = new int [row][column];
+		for(int i=0;i<row;i++){
+			for(int j=0; j<column;j++){
+				a[i][j];
+				count++;
+			}
+		}
+		return a;
 	}
 
 	// Array should be of format
